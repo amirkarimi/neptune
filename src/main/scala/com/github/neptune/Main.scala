8b6a431b1304cecedaec54e030aac75c97e6dc16
Amir Karimi <a.karimi.k@gmail.com>
2017-07-27 21:59:25 +0100

    Add Neptune class and settings and add neptune pixel art

--- src/main/scala/com/github/neptune/Main.scala
+++ src/main/scala/com/github/neptune/Main.scala
@@ -1,21 +1,23 @@
 package com.github.neptune
 
 import org.scalajs.dom
-import dom.document
-import org.scalajs.dom.html._
-import org.scalajs.dom.raw.MouseEvent
+import dom.{ document, window }
+import dom.html._
+import dom.raw.MouseEvent
 import scala.scalajs.js.annotation.JSExportTopLevel
 import scalacss.ProdDefaults._
 
-case class Action(icon: String, title: String, command: () => Unit)
+@JSExportTopLevel("Neptune")
+object Neptune {
+  case class Action(icon: String, title: String, command: () => Unit)
 
-object Act {
-  def apply(icon: String, title: String)(command: => Unit): Action = {
-    Action.apply(icon, title, () => command)
+  object Act {
+    def apply(icon: String, title: String)(command: => Unit): Action = {
+      Action.apply(icon, title, () => command)
+    }
   }
-}
 
-object Main {
+  case class Settings(actions: Seq[Action] = actions, styleWithCss: Boolean = false)
 
   val actions = Seq(
     Act("<b>B</b>", "Bold") {
@@ -26,6 +28,41 @@ object Main {
     },
     Act("<u>U</u>", "Underline") {
       exec("underline")
+    },
+    Act("<strike>S</strike>", "strikeThrough") {
+      exec("strikeThrough")
+    },
+    Act("<b>H<sub>1</sub></b>", "Heading 1") {
+      exec("formatBlock", "<H1>")
+    },
+    Act("<b>H<sub>2</sub></b>", "Heading 2") {
+      exec("formatBlock", "<H2>")
+    },
+    Act("&#182;", "Paragraph") {
+      exec("formatBlock", "<P>")
+    },
+    Act("&#8220; &#8221;", "Quote") {
+      exec("formatBlock", "<BLOCKQUOTE>")
+    },
+    Act("&#35;", "Ordered List") {
+      exec("insertOrderedList")
+    },
+    Act("&#8226;", "Unordered List") {
+      exec("insertUnorderedList")
+    },
+    Act("&lt;/&gt;", "Code") {
+      exec("formatBlock", "<PRE>")
+    },
+    Act("&#8213;", "Horizontal Line") {
+      exec("insertHorizontalRule")
+    },
+    Act("&#128279;", "Link") {
+      val url = window.prompt("Enter the link URL")
+      if (url.nonEmpty) exec("createLink", url)
+    },
+    Act("&#128247;", "Image") {
+      val url = window.prompt("Enter the image URL")
+      if (url.nonEmpty) exec("insertImage", url)
     }
   )
 
@@ -37,9 +74,8 @@ object Main {
     document.execCommand(command, false, value)
   }
 
-
   @JSExportTopLevel("neptune")
-  def neptune(element: dom.Element): Unit = {
+  def neptune(element: dom.Element, settings: Settings = Settings()): Html = {
     element match {
       case element: dom.html.Element => element.className = NeptuneStyles.neptune.htmlClass
       case other => // Don't know how to settup className for non-html element
@@ -63,15 +99,22 @@ object Main {
       button.onclick = { (e: MouseEvent) => action.command() }
       actionbar.appendChild(button)
     }
+
+    if (settings.styleWithCss) exec("styleWithCSS")
+
+    content
   }
 
   val preventTab: (dom.KeyboardEvent => Any) = { e => 
     if (e.keyCode == 9) e.preventDefault()
   }
+}
+
+object Main {
 
   def main(args: Array[String]): Unit = {
     NeptuneStyles.addToDocument()
     // TODO
-    neptune(document.getElementById("my-editor"))
+    Neptune.neptune(document.getElementById("my-editor"))
   }
 }
